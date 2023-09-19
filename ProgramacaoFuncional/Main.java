ae30aa813a23de766b574b4ab684935e2f4211a4
lichston <vitor.joao04@edu.pucrs.br>
2023-09-19 15:05:36 -0300

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   ProgramacaoFuncional/Main.java
    	new file:   ProgramacaoFuncional/aula01/Main.java
    	new file:   ProgramacaoFuncional/aula01/MyComparator.java
    	new file:   ProgramacaoFuncional/aula01/Product.java
    	new file:   ProgramacaoFuncional/aula02/Program.java

---
+++ ProgramacaoFuncional/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import aula01.Product;
+import aula02.Program;
+
+public class Main {
+
+
+
+
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+        list.sort(Main::compareProducts);
+        list.forEach(System.out::println);
+
+}
+
+    public static int compareProducts(Product p1, Product p2) {
+
+        return p1.getPrice().compareTo(p2.getPrice());
+    
+    }
+    
+}
